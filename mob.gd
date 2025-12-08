ef5fa434ef2326a9bb49c1c4efc4938dedfd5b35
Adam Molnar <adam@molnar.click>
2025-12-07 19:15:05 -0500

    Added enemy animations

--- mob.gd
+++ mob.gd
@@ -12,9 +12,11 @@ func initialize(start_position, player_position):
 	look_at_from_position(start_position, player_position, Vector3.UP)
 	rotate_y(randf_range(-PI / 4, PI / 4))
 	
-	var random_seed = randi_range(min_speed, max_speed)
-	velocity = Vector3.FORWARD * random_seed
+	var random_speed = randi_range(min_speed, max_speed)
+	velocity = Vector3.FORWARD * random_speed
 	velocity = velocity.rotated(Vector3.UP, rotation.y)
+	
+	$AnimationPlayer.speed_scale = random_speed / min_speed
 
 func _on_visible_on_screen_notifier_3d_screen_exited() -> void:
 	queue_free()
